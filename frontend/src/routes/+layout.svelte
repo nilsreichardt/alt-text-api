dea107733e315fbdba4fee1dabd717c8b16835db
GeneralMine <marvin@raiser.dev>
2023-10-12 20:32:56 +0200

    Init frontend

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<slot />
+
+<style>
+	:global(body) {
+		width: 800px;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 2rem;
+	}
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { writeFile } from 'fs/promises';
+	let uploadPath;
+
+	$: if (uploadPath) {
+		console.log(uploadPath);
+		const file = data.get('file');
+		const result = await file;
+		console.log('Read the file:', result);
+		const readFile = new Uint8Array(await file.arrayBuffer());
+		console.log('Read the file:', result);
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<input type="file" accept="image/png, image/jpeg" bind:value={uploadPath} />
+
+<style>
+	input {
+		width: 100px;
+		height: 50px;
+	}
+</style>
