0226cba1f3ce81ca4585e4fdcdf46d5b232bdbf0
nilsreichardt <me@nils.re>
2023-10-12 16:11:48 +0200

    Add basic cloud run service

---
+++ backend/cloud_run/bin/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PROJECT=alt-text-api
+IMAGE=gcr.io/${PROJECT}/alt-text-api
+
+env_vars="ENV=production,"
+env_vars="${env_vars}VERSION=${VERSION},"
+
+gcloud builds submit --tag $IMAGE --project=$PROJECT --gcs-log-dir=gs://${PROJECT}_cloudbuild/logs && \
+gcloud run deploy alt-text-api \
+  --image $IMAGE \
+  --set-env-vars="$env_vars" \
+  --project=$PROJECT \
+  --platform managed \
+  --allow-unauthenticated \
+  --region=europe-west1
